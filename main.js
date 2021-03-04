6b80f33c77f406945bd99ae083257376815ab5a7
Prana Tantri <67408817+ProgrammingWithPranav@users.noreply.github.com>
2021-03-04 15:51:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
 Webcam.set({
-    width: 310,
-    height: 300,
-    image_format: 'png',
-    png_quality: 900
-})
+  width: 300,
+  height: 300,
+  image_format: "png",
+  png_quality: 900,
+  constraints: {
+    facingMode: "environment",
+  },
+});
 Webcam.attach("webcam");
+
+function captureImage() {
+  Webcam.snap(function (data_uri) {
+    document.getElementById("snapshot").innerHTML =
+      '<img id="myImg" src="' + data_uri + '" />';
+  });
+}
+
+classifier = ml5.imageClassifier("MobileNet", modelLoaded);
+
+function modelLoaded() {
+  console.log("Model Loaded");
+}
+
+function check() {
+  myImg = document.getElementById("myImg");
+  classifier.classify(myImg, anyName);
+}
+
+function anyName(error, results) {
+    if(error == null) {
+        document.getElementById("resultValue").innerHTML = results[0].label;
+    }
+}
